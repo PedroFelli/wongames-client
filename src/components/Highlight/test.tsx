bf4ad92f6f7e2e7dc59d04ab5fb47951838244f2
Pedro Fellipe <pedrofellipeunai@gmail.com>
2020-11-16 21:07:00 -0300

    Highlight left content

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Highlight from '.'
+import * as S from './styles'
 
 const props = {
   title: 'Heading 1',
@@ -40,6 +41,33 @@ describe('<Highlight />', () => {
     expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
       'src',
       '/float-image.png'
+    )
+  })
+
+  it('should render align right by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    )
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content}`
+    })
+  })
+
+  it('should render align left', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content}`
+    })
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
     )
   })
 })
