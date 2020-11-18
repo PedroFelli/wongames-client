585a05e3e7cb142275476365b4a988d1455bf8fe
Pedro Fellipe <pedrofellipeunai@gmail.com>
2020-11-17 22:08:52 -0300

    favorite icon

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -54,4 +54,10 @@ describe('<GameCard />', () => {
       textDecoration: 'line-through'
     })
   })
+
+  it('should render a filleed Favorit icon when favorite is true', () => {
+    renderWithTheme(<GameCard {...props} favorite />)
+
+    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
+  })
 })
